5f7429c56c67bd88f8188b5baf7bbb110f6b19ed
Jason Wong <jasonkwh@gmail.com>
2016-09-29 10:38:06 +1000

    Update shell script

--- update.sh
+++ update.sh
@@ -11,5 +11,7 @@ rm -Rf update.sh
 git clone https://github.com/jasonkwh/sankofa-family/
 cd sankofa-family/
 mv ./* ..
+cd ..
+rm -Rf sankofa-family/
 chmod 755 ./update.sh
 echo "Update completed"
