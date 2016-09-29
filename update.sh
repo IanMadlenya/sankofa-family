9d183fc3367f1c85aa438f23062ba4dbaaebaf75
Jason Wong <jasonkwh@gmail.com>
2016-09-29 10:34:41 +1000

    Add update shell script

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -Rf css/
+rm -Rf fonts/
+rm -Rf images/
+rm -Rf index.html
+rm -Rf js/
+rm -Rf overlays/
+rm -Rf README.md
+rm -Rf services.html
+rm -Rf update.sh
+git clone https://github.com/jasonkwh/sankofa-family/
+cd sankofa-family/
+mv ./* ..
+chmod 755 ./update.sh
+echo "Update completed"
