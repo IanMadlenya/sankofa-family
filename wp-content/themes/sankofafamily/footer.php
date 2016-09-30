4c38f9048d99305c8d81f6cd27474c2d0df363f8
Jason Wong <jasonkwh@gmail.com>
2016-09-30 11:37:25 +1000

    update wordpress

--- wp-content/themes/sankofafamily/footer.php
+++ wp-content/themes/sankofafamily/footer.php
@@ -1,6 +1,13 @@
-<div id="footer">
-<h1>FOOTER</h1>
+<!-- Footer -->
+<div class="footer-text w3-round-large w3-hover-black">
+<p><img src="/images/warning.png"> Disclaimer: Any general advice in this material does not take into account you or your client‘s personal objectives, financial situation and needs. Please seek advice from a financial adviser or broker and read the relevant IM before making a decision in relation to any investment. In the event of any inconsistency or misinterpretation between the marketing material and SMFOs Pty Ltd.</p>
 </div>
-</div>
+
+<footer class="w3-padding-12 w3-transparent">
+    <a href="http://www.sankofafund.com.au"><img src="/images/logo.png"></a>
+    <p class="w3-left-align">© 2016 <strong>SMFOs Pty Ltd</strong> (ACN 613532835), All rights reserved.</p>
+</footer>
+<script src="/js/back.to.top.js"></script>
+<a href="#0" class="cd-top">Top</a>
 </body>
 </html>
