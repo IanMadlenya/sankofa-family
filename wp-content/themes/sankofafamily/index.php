19e506deb6c71bbe853aca08b4564657a77ad24e
Jason Wong <jasonkwh@gmail.com>
2016-10-04 10:40:05 +1100

    update display format

--- wp-content/themes/sankofafamily/index.php
+++ wp-content/themes/sankofafamily/index.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
 <div class="w3-content w3-container w3-text-dark-grey">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<h1><?php the_title(); ?></h1>
-<h4>Posted on <?php the_time('F jS, Y') ?></h4>
 <p><?php the_content(__('(more...)')); ?></p>
 <hr> <?php endwhile; else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
