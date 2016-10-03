84e654799753638744e7df7c014ecfb11423ad6c
Jason Wong <jasonkwh@gmail.com>
2016-10-04 10:37:47 +1100

    update nabber style

--- wp-content/themes/sankofafamily/header.php
+++ wp-content/themes/sankofafamily/header.php
@@ -14,11 +14,11 @@
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <ul class="w3-navbar" id="myNavbar">
-    <li><a href="/index.html" class="w3-padding-large w3-text-light-grey">首页</a></li>
-      <li><a href="/services.html" class="w3-padding-large w3-text-light-grey">产品信息</a></li>
-      <li><a href="#" class="w3-padding-large w3-text-light-grey">什么是家族办公室</a></li>
-      <li><a href="/index.html#our-team" class="w3-padding-large w3-text-light-grey">团队介绍</a></li>
-      <li><a href="/index.html#sankofa-contact" class="w3-padding-large w3-text-light-grey">联系我们</a></li>
+    <li><a href="/index.html" class="w3-padding-large w3-text-dark-grey">首页</a></li>
+      <li><a href="/services.html" class="w3-padding-large w3-text-dark-grey">产品信息</a></li>
+      <li><a href="#" class="w3-padding-large w3-text-dark-grey">什么是家族办公室</a></li>
+      <li><a href="/index.html#our-team" class="w3-padding-large w3-text-dark-grey">团队介绍</a></li>
+      <li><a href="/index.html#sankofa-contact" class="w3-padding-large w3-text-dark-grey">联系我们</a></li>
     <li class="w3-hide-small w3-right">
       <a href="#" class="w3-padding-large w3-hover-red"><i class="fa fa-search"></i></a>
     </li>
