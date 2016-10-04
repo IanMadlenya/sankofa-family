099736bbe99040851c00dde0c48d5bba93de0f37
Jason Wong <jasonkwh@gmail.com>
2016-10-04 15:02:42 +1100

    update index.php

--- wp-content/themes/sankofafamily/index.php
+++ wp-content/themes/sankofafamily/index.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
-<div class="w3-content w3-container w3-text-dark-grey">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <p><?php the_content(__('(more...)')); ?></p>
 <hr> <?php endwhile; else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-</div>
 <?php get_footer(); ?>
