5b1562e59dec903344dcfc55db87cf6a3ba92db6
Jason Wong <jasonkwh@gmail.com>
2016-10-07 15:51:17 +1100

    fix shell script 2

--- fix_install.sh
+++ fix_install.sh
@@ -8,6 +8,6 @@ cd ..
 rm -Rf sankofa-family/
 chmod 755 ./update.sh
 chmod 755 ./fix_install.sh
-chmod 717 ./wp-content/uploads
+chmod 717 wp-content/uploads/
 cp -f ./fix_install.sh ..
 echo "Fix completed"
