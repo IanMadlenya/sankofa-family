b93649c43f7e4dbd0820f613d73b868e0e5b1c28
Jason Wong <jasonkwh@gmail.com>
2016-10-14 12:21:39 +1100

    fix linking resources

--- wp-content/themes/sankofafamily/services.php
+++ wp-content/themes/sankofafamily/services.php
@@ -10,13 +10,13 @@ $current_user = wp_get_current_user();
 <title>Sankofa 家族办公室</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
-<link rel="stylesheet" href="./css/w3.css">
-<link rel="stylesheet" href="./css/font-awesome.min.css">
-<link rel="stylesheet" href="./css/hexagon.css">
-<link rel="stylesheet" href="./css/style.css">
-<link rel="stylesheet" href="./css/vegas.min.css">
-<script src="./js/jquery.min.js"></script>
-<script src="./js/vegas.min.js"></script>
+<link rel="stylesheet" href="/css/w3.css">
+<link rel="stylesheet" href="/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/hexagon.css">
+<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="/css/vegas.min.css">
+<script src="/js/jquery.min.js"></script>
+<script src="/js/vegas.min.js"></script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 </head>
 <body>
@@ -137,15 +137,15 @@ $current_user = wp_get_current_user();
     
 <!-- Footer -->
 <div class="footer-text w3-round-large w3-hover-black">
-<p><img src="./images/warning.png"> Disclaimer: Any general advice in this material does not take into account you or your client‘s personal objectives, financial situation and needs. Please seek advice from a financial adviser or broker and read the relevant IM before making a decision in relation to any investment. In the event of any inconsistency or misinterpretation between the marketing material and SMFOs Pty Ltd.</p>
+<p><img src="/images/warning.png"> Disclaimer: Any general advice in this material does not take into account you or your client‘s personal objectives, financial situation and needs. Please seek advice from a financial adviser or broker and read the relevant IM before making a decision in relation to any investment. In the event of any inconsistency or misinterpretation between the marketing material and SMFOs Pty Ltd.</p>
 </div>
 
 <footer class="w3-padding-12 w3-transparent">
-    <a href="http://www.sankofafund.com.au"><img src="./images/logo.png"></a>
+    <a href="http://www.sankofafund.com.au"><img src="/images/logo.png"></a>
     <p class="w3-left-align w3-text-white">© 2016 <strong>SMFOs Pty Ltd</strong> (ACN 613532835), All rights reserved.</p>
 </footer>
-<script src="./js/services.js"></script>
-<script src="./js/back.to.top.js"></script>
+<script src="/js/services.js"></script>
+<script src="/js/back.to.top.js"></script>
 <a href="#0" class="cd-top">Top</a>
 </body>
 </html>
