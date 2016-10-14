1e9d8c8c20390954f7a505807c55d5cd9c45ba90
Jason Wong <jasonkwh@gmail.com>
2016-10-14 13:17:58 +1100

    update style 5

--- wp-content/themes/sankofafamily/header.php
+++ wp-content/themes/sankofafamily/header.php
@@ -24,9 +24,9 @@ $current_user = wp_get_current_user();
       <li><a href="/index.html#sankofa-contact" class="w3-padding-large w3-text-dark-grey">联系我们</a></li>
     <li class="w3-hide-small w3-right">
       <?php if ( is_user_logged_in() ): ?>
-        <a href="#" class="w3-padding-large w3-hover-green w3-text-dark-grey"><?php echo $current_user->user_login ?></a>
+        <a href="#" class="w3-padding-large w3-black w3-hover-green w3-text-light-grey"><?php echo $current_user->user_login ?></a>
         <?php else: ?>
-      <a href="/wp-admin" class="w3-padding-large w3-hover-green w3-text-dark-grey">登录</a>
+      <a href="/wp-admin" class="w3-padding-large w3-black w3-hover-green w3-text-light-grey">登录</a>
         <?php endif; ?>
     </li>
   </ul>
