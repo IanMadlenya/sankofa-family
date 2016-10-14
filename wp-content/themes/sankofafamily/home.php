71f2ec8e3c0542168578d95a3908500467dd5cfd
Jason Wong <jasonkwh@gmail.com>
2016-10-14 16:13:32 +1100

    try using buttons instead of hyperlinks

--- wp-content/themes/sankofafamily/home.php
+++ wp-content/themes/sankofafamily/home.php
@@ -29,7 +29,7 @@ $current_user = wp_get_current_user();
     <li class="w3-hide-small w3-right">
       <?php if ( is_user_logged_in() ): ?>
         <div class="sankofa-dropdown">
-  <a href="/wp-admin" class="sankofa-dropbtn w3-padding-large w3-hover-green w3-text-light-grey"><?php echo $current_user->user_login ?></a>
+  <button class="sankofa-dropbtn w3-padding-large w3-hover-green w3-text-light-grey"><?php echo $current_user->user_login ?></button>
   <div class="sankofa-dropdown-content">
     <a href="#">Link 1</a>
     <a href="#">Link 2</a>
