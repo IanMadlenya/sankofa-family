331c3af24f946e57da68b1d7c0920ffee644c8d4
Jason Wong <jasonkwh@gmail.com>
2016-10-17 11:14:35 +1100

    update text style

--- wp-content/themes/sankofafamily/portal.php
+++ wp-content/themes/sankofafamily/portal.php
@@ -27,7 +27,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
     <h4 class="w3-padding-0"><b><?php echo $current_user->user_login ?></b></h4>
     <p class="w3-text-grey"><?php echo $current_user->user_email ?></p>
   </div>
-  <a href="#portfolio" onclick="w3_close()" class="w3-padding w3-text-teal">编辑个人资料</a>
+  <a href="#portfolio" onclick="w3_close()" class="w3-padding w3-text-green">编辑个人资料</a>
   <a href="#about" onclick="w3_close()" class="w3-padding">上传申请表</a>
   <a href="#contact" onclick="w3_close()" class="w3-padding">登出</a>
    
