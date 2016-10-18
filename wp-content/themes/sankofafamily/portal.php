6a82eb43c7722d3f28bb9664caab5517a7973138
Jason Wong <jasonkwh@gmail.com>
2016-10-18 15:10:46 +1100

    update navbar style

--- wp-content/themes/sankofafamily/portal.php
+++ wp-content/themes/sankofafamily/portal.php
@@ -58,15 +58,15 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 <div class="w3-top">
   <ul class="w3-navbar" id="myNavbar">
     <li class="w3-right">
-        <a href="/" class="w3-padding-large w3-text-light-grey">首页</a></li>
+        <a href="/" class="w3-padding-large w3-text-dark-grey">首页</a></li>
       <li class="w3-right">
-        <a href="/services" class="w3-padding-large w3-text-light-grey">产品信息</a></li>
+        <a href="/services" class="w3-padding-large w3-text-dark-grey">产品信息</a></li>
       <li class="w3-right">
-        <a href="#" class="w3-padding-large w3-text-light-grey">什么是家族办公室</a></li>
+        <a href="#" class="w3-padding-large w3-text-dark-grey">什么是家族办公室</a></li>
       <li class="w3-right">
-        <a href="/#our-team" class="w3-padding-large w3-text-light-grey">团队介绍</a></li>
+        <a href="/#our-team" class="w3-padding-large w3-text-dark-grey">团队介绍</a></li>
       <li class="w3-right">
-        <a href="/#sankofa-contact" class="w3-padding-large w3-text-light-grey">联系我们</a></li>
+        <a href="/#sankofa-contact" class="w3-padding-large w3-text-dark-grey">联系我们</a></li>
   </ul>
 </div>
   
