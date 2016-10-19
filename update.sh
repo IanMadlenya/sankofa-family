c0c0cf32b38cb790ffca985a7ec21dc8830893a7
Jason Wong <jasonkwh@gmail.com>
2016-10-19 23:56:31 +1100

    Fix update shell script
    and remove owncloud

--- update.sh
+++ update.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-rm -Rf ./*
+shopt -s extglob
+cd wp-content/
+rm -Rf !(uploads)
+cd ..
+rm -Rf !(wp-content)
 git clone https://github.com/jasonkwh/sankofa-family/
 cd sankofa-family/
-mv ./* ..
+cp -Rf ./* ..
 cd ..
 rm -Rf sankofa-family/
 rm -Rf localhost.sql
 chmod 755 ./update.sh
 chmod 755 ./fix_install.sh
-chmod 717 wp-content/uploads/
 cp -f ./fix_install.sh ..
 echo "Update completed"
