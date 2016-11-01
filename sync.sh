859c8bd8cd6f68d11b800be541d6330b95638d21
Jason Wong <jasonkwh@gmail.com>
2016-11-01 13:40:18 +1100

    update shell script

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ssh root@47.90.81.198 "find /data/wwwroot/default/wp-content/uploads/pdfs -type f -exec rm {} \;"
 scp -r ~/Desktop/pdf/* root@47.90.81.198:/data/wwwroot/default/wp-content/uploads/pdfs
+ssh root@47.90.81.198 "chmod -R 755 /data/wwwroot/default/wp-content/uploads/pdfs"
