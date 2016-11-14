2d65e7a443f4b3b143339effff28195442c44f4f
Jason Wong <jasonkwh@gmail.com>
2016-11-14 17:31:00 +1100

    Routine backup Nov 14

--- wp-content/themes/sankofafamily/portal.php
+++ wp-content/themes/sankofafamily/portal.php
@@ -8,7 +8,7 @@ if ( is_user_logged_in() ):
 ?>
 <!DOCTYPE html>
 <html>
-<title>Sankofa 家族办公室</title>
+<title>SankofaCRM</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/css/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
@@ -30,7 +30,7 @@ if ( is_user_logged_in() ):
     <h4 class="w3-padding-0"><b><?php echo $current_user->user_login ?></b></h4>
     <p class="w3-text-grey"><?php echo $current_user->user_email ?></p>
   </div>
-  <a href="#portfolio" onclick="w3_close()" class="w3-padding w3-text-green w3-hover-text-light-grey">编辑个人资料</a>
+  <a href="/portal" onclick="w3_close()" class="w3-padding w3-text-green w3-hover-text-light-grey">主页</a>
   <a href="/class" onclick="w3_close()" class="w3-padding w3-hover-text-light-grey">微信课堂登记</a>
 <?php if ( array_shift( $current_user->roles ) == "administrator" ): ?>
 <a href="/class-view" onclick="w3_close()" class="w3-padding w3-hover-text-light-grey">微信课堂登记数据</a>
@@ -44,26 +44,7 @@ if ( is_user_logged_in() ):
 
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:300px">
-    
-<!-- Navbar (sit on top) -->
-<div class="w3-top">
-  <ul class="w3-navbar" id="myNavbar">
-    <li><a href="/" class="w3-padding-large w3-text-dark-grey">首页</a></li>
-      <li><a href="/services" class="w3-padding-large w3-text-dark-grey">产品信息</a></li>
-      <li><a href="#" class="w3-padding-large w3-text-dark-grey">什么是家族办公室</a></li>
-      <li><a href="/#our-team" class="w3-padding-large w3-text-dark-grey">团队介绍</a></li>
-      <li><a href="/#sankofa-contact" class="w3-padding-large w3-text-dark-grey">联系我们</a></li>
-  </ul>
-</div>
-<!-- Footer -->
-<div class="footer-text w3-round-large w3-hover-black">
-<p><img src="/images/warning.png"> Disclaimer: Any general advice in this material does not take into account you or your client‘s personal objectives, financial situation and needs. Please seek advice from a financial adviser or broker and read the relevant IM before making a decision in relation to any investment. In the event of any inconsistency or misinterpretation between the marketing material and SMFOs Pty Ltd.</p>
-</div>
 
-<footer class="w3-padding-12 w3-transparent">
-    <a href="http://www.sankofafund.com.au"><img src="/images/logo_black.png"></a>
-    <p class="w3-left-align">© 2016 <strong>SMFOs Pty Ltd</strong> (ACN 613532835), All rights reserved.</p>
-</footer>
 <a href="#0" class="cd-top">Top</a>
 
 <!-- End page content -->
