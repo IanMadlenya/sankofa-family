bd70a4b1450a4ab9ebdedd9c72e144aa35a81976
Jason Wong <jasonkwh@gmail.com>
2016-11-15 19:27:55 +1100

    Start user interface design

--- wp-content/themes/sankofafamily/portal.php
+++ wp-content/themes/sankofafamily/portal.php
@@ -45,6 +45,26 @@ if ( is_user_logged_in() ):
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:300px">
 
+<!-- Header -->
+<header class="w3-container w3-right-align crm-title-padding">
+    <h3>欢迎您回来, <b><?php echo $current_user->user_login ?></b>!</h3>
+</header>
+    
+<!-- Labels / tags -->
+  <div class="w3-card-4 w3-margin">
+    <div class="w3-container w3-padding">
+      <h5>个人资料</h5>
+    </div>
+    <div class="w3-container w3-white">
+    <p><span class="w3-tag w3-black w3-margin-bottom">Travel</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">New York</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">London</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">IKEA</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">NORWAY</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">DIY</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Ideas</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Baby</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Family</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">News</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Clothing</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Shopping</span>
+      <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Sports</span> <span class="w3-tag w3-light-grey w3-small w3-margin-bottom">Games</span>
+    </p>
+    </div>
+  </div>
+
 <a href="#0" class="cd-top">Top</a>
 
 <!-- End page content -->
