da49a4667d1c579a4ae30fccf4fb84146c6dd1e9
Jason Wong <jasonkwh@gmail.com>
2016-11-25 17:43:50 +1100

    Routine backup 25 Nov 2016
    Start working on English site.

--- wp-content/themes/sankofafamily/home-en.php
+++ wp-content/themes/sankofafamily/home-en.php
@@ -1,7 +1,12 @@
+<?php
+/*
+Template Name: sankofa-home-en
+*/
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Sankofa 家族办公室</title>
+<title>Sankofa Multi-Family Offices</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
 <link rel="stylesheet" href="/css/w3.css">
@@ -15,13 +20,13 @@
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <ul class="w3-navbar" id="myNavbar">
-    <li><a href="/" class="w3-padding-large w3-text-light-grey">首页</a></li>
-      <li><a href="/services" class="w3-padding-large w3-text-light-grey">产品信息</a></li>
-      <li><a href="#" class="w3-padding-large w3-text-light-grey">什么是家族办公室</a></li>
-      <li><a href="/our-team" class="w3-padding-large w3-text-light-grey">团队介绍</a></li>
-      <li><a href="/#sankofa-contact" class="w3-padding-large w3-text-light-grey">联系我们</a></li>
+    <li><a href="/en" class="w3-padding-large w3-text-light-grey">Home</a></li>
+      <li><a href="/services-en" class="w3-padding-large w3-text-light-grey">Products</a></li>
+      <li><a href="#" class="w3-padding-large w3-text-light-grey">Definition of MFOs</a></li>
+      <li><a href="/our-team-en" class="w3-padding-large w3-text-light-grey">Our team</a></li>
+      <li><a href="/en#sankofa-contact" class="w3-padding-large w3-text-light-grey">Contact us</a></li>
     <li class="w3-hide-small w3-right">
-      <a href="/en" class="w3-padding-large w3-hover-green w3-text-light-grey">English</a>
+      <a href="/" class="w3-padding-large w3-hover-green w3-text-light-grey">中文</a>
     </li>
   </ul>
 </div>
@@ -29,35 +34,35 @@
 <!-- First Parallax Image with Logo Text -->
 <div class="bgimg-1 w3-opacity w3-display-container">
   <div class="w3-display-middle" style="white-space:nowrap;">
-      <span class="w3-xxlarge w3-text-light-grey w3-wide w3-animate-opacity"><a href="http://www.sankofafund.com.au"><img class="sankofa-logo" src="/images/logo.png"></a> 家族办公室</span>
+      <span class="w3-xxlarge w3-text-light-grey w3-animate-opacity"><a href="http://www.sankofafund.com.au"><img class="sankofa-logo" src="/images/logo.png"></a> Multi-Family Offices</span>
   </div>
 </div>
 
 <!-- Sign Up -->
-<a class="w3-btn w3-light-grey w3-hover-black w3-medium w3-display-middle sankofa-sign-up" href="/class">登记微信课堂</a>
+<a class="w3-btn w3-light-grey w3-hover-black w3-medium w3-display-middle sankofa-sign-up" href="/class">Sign up WeChat class</a>
 
 <!-- Container (About Section) -->
 <div class="w3-content w3-container w3-padding-64" id="about">
-    <h3 class="w3-center">关于 Sankofa 家族办公室</h3>
-  <p>Sankofa 家族办公室（Sankofa Multi-Family Offices 简称：SMFOs）是隶属于澳大利亚Sankofa基金管理有限公司(ACN: 602 218 495 AFSL: 473 202)专门服务高端客户，海外资产配置、家族信托、移民、留学、安家、置业的专业服务机构。自其成立以来，SMFOs已为数个财富家庭成立了家族信托，并帮助他们完成全家移民，子女留学，海外资产配置等诉求。</p>
+    <h3 class="w3-center">About Sankofa Multi-Family Offices</h3>
+  <p>Sankofa Multi-Family Offices, or SMFOs, 是隶属于澳大利亚Sankofa基金管理有限公司(ACN: 602 218 495 AFSL: 473 202)专门服务高端客户，海外资产配置、家族信托、移民、留学、安家、置业的专业服务机构。自其成立以来，SMFOs已为数个财富家庭成立了家族信托，并帮助他们完成全家移民，子女留学，海外资产配置等诉求。</p>
   <div class="w3-row">
     <div class="w3-col m6 w3-center w3-section">
       <img src="/images/george_gao.jpg" class="george-img w3-round-large" alt="George Gao, Sankofa家族信托办公室 董事总经理">
-        <p class="sankofa-title">George Gao, Sankofa 家族信托办公室</p>
-        <p class="sankofa-title-below">董事总经理</p>
+        <p class="sankofa-title">George Gao, Sankofa Multi-Family Offices</p>
+        <p class="sankofa-title-below">Managing Director</p>
     </div>
 
     <!-- Hide this text on small devices -->
     <div class="w3-col m6 w3-hide-small w3-section">
       <p>家族办公室始于6世纪，当时国王的管家负责管理皇家财富。后来贵族要求参与其中，自此管理的概念应运而生并延续至今。现代意义上的家族办公室在19世纪得到发展。1838 年，作为金融家和艺术收藏家 J.P. 摩根家族创立了摩根财团来管理家族资产。1882年，洛克菲勒家族设立了自己的家族办公室。该家族办公室至 今仍在运营，并为其他家族提供服务。家族办公室涵盖管理私人大宗财富的各种组织与服务形式...</p>
-        <a class="w3-btn w3-hover-light-grey w3-medium w3-left sankofa-middle" href="/sankofa-family">了解更多</a>
+        <a class="w3-btn w3-hover-light-grey w3-medium w3-left sankofa-middle" href="/sankofa-family">Read more</a>
     </div>
   </div>
 </div>
     
 <!-- Second Parallax Image with Portfolio Text -->
 <div class="bgimg-2 w3-display-container" id="our-team">
-<div class="w3-xxlarge w3-text-light-grey w3-wide w3-center sankofa-team-text">我们的团队</div>
+<div class="w3-xxlarge w3-text-light-grey w3-wide w3-center sankofa-team-text">Our team</div>
 <div class="w3-row-padding w3-grayscale" style="padding-top:120px">
     <div class="w3-col l205 w3-margin-bottom">
     </div>
@@ -66,8 +71,8 @@
         <img src="/images/george_gao2.png" style="width:100%;min-width:250px">
         <div class="w3-container w3-white" style="height:260px;width:100%;min-width:250px">
           <h4>George Gao</h4>
-          <p class="w3-opacity w3-small">SMFOs 董事总经理</p>
-          <p class="w3-small">高先生有着20年金融证券外汇从业和投资经验，历任中国投资银行，中信证券资产管理部交易员，长盛基金，华夏基金，融通基金，KVB，易汇通金融学院院长。持有注册会计师、注册证券分析师、澳洲RG146金融从业牌照。</p>
+          <p class="w3-opacity w3-small">SMFOs Managing Director</p>
+          <p class="w3-small">George has more than 20 years’ experience in the financial industry. He served at China Investment Bank and CITIC Sercurities as a senior trader. His titles include CPA, CRFA and VP of EFX168 Financial College.</p>
         </div>
       </div>
     </div>
@@ -76,7 +81,7 @@
         <img src="/images/gracie2.png" style="width:100%;min-width:250px">
         <div class="w3-container w3-white" style="height:260px;width:100%;min-width:250px">
           <h4>Gracie He</h4>
-          <p class="w3-opacity w3-small">SMFOs 首席财务官</p>
+          <p class="w3-opacity w3-small">SMFOs Chief Financial Officer</p>
           <p class="w3-small">澳大利亚注册会计师CPA，特许税务师 CTA，注册税务代理 Registered tax agent ，注册会计师导师 ，超过10年的澳大利亚会计和税务从业经验，擅长中小型企业税务规划，企业业务结构重组，企业跨境税务规划，高净值客户的个人税务理财等。</p>
         </div>
       </div>
@@ -86,7 +91,7 @@
         <img src="/images/vivienne2.png" style="width:100%;min-width:250px">
         <div class="w3-container w3-white" style="height:260px;width:100%;min-width:250px">
           <h4>Vivienne Goodwin</h4>
-          <p class="w3-opacity w3-small">SMFOs 首席大律师</p>
+          <p class="w3-opacity w3-small">SMFOs Chief Legal Officer</p>
           <p class="w3-small">德盈律师事务所创始人，新闻硕士、法律博士，新南威尔士州执业律师，澳大利亚最高法院执业律师，澳大利亚国际公证律师。专业领域为经济商业法、公司法、房地产开发、项目融资；有十年以上协助商业投资移民客户经验；擅长协助中国企业客户在澳着陆投资；此外还有丰富的仲裁庭经验。</p>
         </div>
       </div>
@@ -95,7 +100,7 @@
 
 <div class="w3-row" style="padding-bottom:30px">
 <div class="w3-center">
-<p><a class="w3-btn w3-light-grey w3-hover-black w3-medium" href="/our-team">查看更多</a></p>
+<p><a class="w3-btn w3-light-grey w3-hover-black w3-medium" href="/our-team-en">More details</a></p>
 </div>
 </div>
 </div>
@@ -113,21 +118,21 @@
       <div class="w3-large w3-margin-bottom">
           <img class="sankofa-logo-dark" src="/images/logo_orig.png"><br>
         <i class="fa fa-map-marker w3-hover-text-black" style="width:30px"></i> Level 40, 19 Martin Pl, Sydney, NSW 2000<br>
-        <i class="fa fa-phone w3-hover-text-black" style="width:30px"></i> 电话: +61 (2) 8065 2830<br>
+        <i class="fa fa-phone w3-hover-text-black" style="width:30px"></i> Phone: +61 (2) 8065 2830<br>
           <i class="fa fa-envelope w3-hover-text-black" style="width:30px"> </i> Email: info@sankofafund.com.au<br>
       </div>
       <p>Swing by for a cup of coffee, or leave me a note:</p>
         <form class="w3-container" method="post" action="/msg-input.php">
       <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
         <div class="w3-half">
-          <input class="w3-input w3-border w3-hover-light-grey" type="text" placeholder="姓名 NAME" name="clientname">
+          <input class="w3-input w3-border w3-hover-light-grey" type="text" placeholder="NAME" name="clientname">
         </div>
         <div class="w3-half">
-        <input class="w3-input w3-border w3-hover-light-grey" type="text" placeholder="电子邮件 EMAIL" name="email">
+        <input class="w3-input w3-border w3-hover-light-grey" type="text" placeholder="EMAIL" name="email">
         </div>
       </div>
-      <input class="w3-input w3-border w3-hover-light-grey" type="text" placeholder="留言 MESSAGE" name="message">
-    <input type="submit" class="w3-btn w3-hover-light-grey w3-section w3-right" value="提交">
+      <input class="w3-input w3-border w3-hover-light-grey" type="text" placeholder="MESSAGE" name="message">
+    <input type="submit" class="w3-btn w3-hover-light-grey w3-section w3-right" value="Submit">
           </form>
     </div>
   </div>
--- wp-content/themes/sankofafamily/services-en.php
+++ wp-content/themes/sankofafamily/services-en.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Template Name: sankofa-services
+Template Name: sankofa-services-en
 */
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Sankofa 家族办公室</title>
+<title>Sankofa Multi-Family Offices</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
 <link rel="stylesheet" href="/css/w3.css">
@@ -23,13 +23,13 @@ Template Name: sankofa-services
 <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <ul class="w3-navbar" id="myNavbar">
-    <li><a href="/" class="w3-padding-large w3-text-light-grey">首页</a></li>
-      <li><a href="/services" class="w3-padding-large w3-text-light-grey">产品信息</a></li>
-      <li><a href="#" class="w3-padding-large w3-text-light-grey">什么是家族办公室</a></li>
-      <li><a href="/our-team" class="w3-padding-large w3-text-light-grey">团队介绍</a></li>
-      <li><a href="/#sankofa-contact" class="w3-padding-large w3-text-light-grey">联系我们</a></li>
+    <li><a href="/en" class="w3-padding-large w3-text-light-grey">Home</a></li>
+      <li><a href="/services-en" class="w3-padding-large w3-text-light-grey">Products</a></li>
+      <li><a href="#" class="w3-padding-large w3-text-light-grey">Definition of MFOs</a></li>
+      <li><a href="/our-team-en" class="w3-padding-large w3-text-light-grey">Our team</a></li>
+      <li><a href="/en#sankofa-contact" class="w3-padding-large w3-text-light-grey">Contact us</a></li>
     <li class="w3-hide-small w3-right">
-      <a href="/services-en" class="w3-padding-large w3-hover-green w3-text-light-grey">English</a>
+      <a href="/services" class="w3-padding-large w3-hover-green w3-text-light-grey">中文</a>
     </li>
   </ul>
 </div>
