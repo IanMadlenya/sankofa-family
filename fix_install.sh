092d4121f737294f674c238350bd30745946636f
Jason Wong <jasonkwh@gmail.com>
2017-01-12 10:07:56 +1100

    Update script

--- fix_install.sh
+++ fix_install.sh
@@ -11,6 +11,9 @@ cp -Rf ./* ..
 cd ..
 rm -Rf sankofa-family/
 rm -Rf localhost.sql
+rm -Rf 0.conf
+rm -Rf httpd.conf
+rm -Rf ssl.zip
 chmod 755 ./update.sh
 chmod 755 ./fix_install.sh
 cp -f ./fix_install.sh ..
