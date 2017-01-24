e1e2312167459b1fa136407617fd36ab1cc4a30a
Jason Wong <jasonkwh@gmail.com>
2017-01-24 14:45:19 +1100

    add estore dialog design

--- wp-content/themes/sankofafamily/estore.php
+++ wp-content/themes/sankofafamily/estore.php
@@ -35,7 +35,7 @@ if(!isset($_COOKIE[$cookie_name])) {
 <link rel="stylesheet" href="/css/style.css">
 <script src="/js/jquery.min.js"></script>
 <script>
-var hidden1=false;var hidden2=false;var hidden3=false;setTimeout(function(){if($('.estore-bg').length>0){$('.estore-bg').remove()}},4000);$(function(){$('.estore-item1').click(function(){if(hidden1==false){$('.eitem1').animate({top:"-60px"});$('.estore-item1-hidden').fadeIn(500);hidden2=false;$('.eitem2').animate({top:"0"});$('.estore-item2-hidden').fadeOut(500);hidden3=false;$('.eitem3').animate({top:"0"});$('.estore-item3-hidden').fadeOut(500);hidden1=true}else{$('.eitem1').animate({top:"0"});$('.estore-item1-hidden').fadeOut(500);hidden1=false}});$('.estore-item2').click(function(){if(hidden2==false){$('.eitem2').animate({top:"-60px"});$('.estore-item2-hidden').fadeIn(500);hidden1=false;$('.eitem1').animate({top:"0"});$('.estore-item1-hidden').fadeOut(500);hidden3=false;$('.eitem3').animate({top:"0"});$('.estore-item3-hidden').fadeOut(500);hidden2=true}else{$('.eitem2').animate({top:"0"});$('.estore-item2-hidden').fadeOut(500);hidden2=false}});$('.estore-item3').click(function(){if(hidden3==false){$('.eitem3').animate({top:"-60px"});$('.estore-item3-hidden').fadeIn(500);hidden1=false;$('.eitem1').animate({top:"0"});$('.estore-item1-hidden').fadeOut(500);hidden2=false;$('.eitem2').animate({top:"0"});$('.estore-item2-hidden').fadeOut(500);hidden3=true}else{$('.eitem3').animate({top:"0"});$('.estore-item3-hidden').fadeOut(500);hidden3=false}})});
+var hidden1=!1,hidden2=!1,hidden3=!1;setTimeout(function(){$(".estore-bg").length>0&&$(".estore-bg").remove()},4e3),$(function(){$(".estore-item1").click(function(){0==hidden1?($(".eitem1").animate({top:"-60px"}),$(".estore-item1-hidden").fadeIn(500),hidden2=!1,$(".eitem2").animate({top:"0"}),$(".estore-item2-hidden").fadeOut(500),hidden3=!1,$(".eitem3").animate({top:"0"}),$(".estore-item3-hidden").fadeOut(500),hidden1=!0):($(".eitem1").animate({top:"0"}),$(".estore-item1-hidden").fadeOut(500),hidden1=!1)}),$(".estore-item2").click(function(){0==hidden2?($(".eitem2").animate({top:"-60px"}),$(".estore-item2-hidden").fadeIn(500),hidden1=!1,$(".eitem1").animate({top:"0"}),$(".estore-item1-hidden").fadeOut(500),hidden3=!1,$(".eitem3").animate({top:"0"}),$(".estore-item3-hidden").fadeOut(500),hidden2=!0):($(".eitem2").animate({top:"0"}),$(".estore-item2-hidden").fadeOut(500),hidden2=!1)}),$(".estore-item3").click(function(){0==hidden3?($(".eitem3").animate({top:"-60px"}),$(".estore-item3-hidden").fadeIn(500),hidden1=!1,$(".eitem1").animate({top:"0"}),$(".estore-item1-hidden").fadeOut(500),hidden2=!1,$(".eitem2").animate({top:"0"}),$(".estore-item2-hidden").fadeOut(500),hidden3=!0):($(".eitem3").animate({top:"0"}),$(".estore-item3-hidden").fadeOut(500),hidden3=!1)})}),$(document).ready(function(){$(".estore-btn1").click(function(){$(".estore-dialog1").fadeIn(500)}),$(".estore-btn2").click(function(){$(".estore-dialog2").fadeIn(500)}),$(".estore-btn3").click(function(){$(".estore-dialog3").fadeIn(500)}),$(".estore-dialog-close").click(function(){$(".estore-dialog1").fadeOut(500),$(".estore-dialog2").fadeOut(500),$(".estore-dialog3").fadeOut(500)})});
 </script>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 </head>
@@ -67,25 +67,68 @@ if ( $current_user->exists() ) {
 <a href="#" class="estore-item1"><img src="/images/estore-icon1.png" style="width:45%;margin-bottom:10px" class="w3-circle w3-hover-opacity w3-hover-shadow estore-icon1"></a>
 <h5>Trust Setup Service</h5>
 <div class="estore-item1-hidden w3-white w3-card-2 w3-padding" style="border-radius:4px;opacity:.8"><h4>AU$5000</h4><p>Online Consulting Service for setting up a Discretionary Trust.</p>
-<button class="estore-btn1 w3-padding" style="margin-bottom:10px">Add to cart</button></div>
+<button class="estore-btn1 w3-padding" style="margin-bottom:10px">More details</button></div>
 </div>
 
 <div class="w3-third eitem2">
 <a href="#" class="estore-item2"><img src="/images/estore-icon2.png" style="width:45%;margin-bottom:10px" class="w3-circle w3-hover-opacity w3-hover-shadow estore-icon2"></a>
 <h5>Business Study Service</h5>
 <div class="estore-item2-hidden w3-white w3-card-2 w3-padding" style="border-radius:4px;opacity:.8"><h4>AU$5000</h4><p>Online Consulting Services for people who are interest in visiting Australia for business purposes.</p>
-<button class="estore-btn2 w3-padding" style="margin-bottom:10px">Add to cart</button></div>
+<button class="estore-btn2 w3-padding" style="margin-bottom:10px">More details</button></div>
 </div>
 
 <div class="w3-third eitem3">
 <a href="#" class="estore-item3"><img src="/images/estore-icon3.png" style="width:45%;margin-bottom:10px" class="w3-circle w3-hover-opacity w3-hover-shadow estore-icon3"></a>
 <h5>Expression of Interest</h5>
 <div class="estore-item3-hidden w3-white w3-card-2 w3-padding" style="border-radius:4px;opacity:.8"><h4>AU$5000</h4><p>Taking your EOI as your deposit in advance for securing your privilege of our consulting services.</p>
-<button class="estore-btn3 w3-padding" style="margin-bottom:10px">Add to cart</button></div>
+<button class="estore-btn3 w3-padding" style="margin-bottom:10px">More details</button></div>
 </div>
 
 </div>
 </div>
+
+<div class="estore-dialog1">
+<div class="estore-dialog-content1">
+<img src="/images/estore-icon1c.png" style="width:20%;margin-top:3%" class="w3-circle estore-icon">
+<a href="#" class="estore-dialog-close w3-hover-opacity"><img src="/images/close.png" style="width:25px"></a>
+<div class="estore-dialog-text">
+<h4>Trust Setup Service</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+</div>
+<div class="w3-center">
+<button class="estore-btn1c w3-padding">Add to cart</button>
+</div>
+</div>
+</div>
+
+<div class="estore-dialog2">
+<div class="estore-dialog-content2">
+<img src="/images/estore-icon2c.png" style="width:20%;margin-top:3%" class="w3-circle estore-icon">
+<a href="#" class="estore-dialog-close w3-hover-opacity"><img src="/images/close.png" style="width:25px"></a>
+<div class="estore-dialog-text">
+<h4>Business Study Service</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+</div>
+<div class="w3-center">
+<button class="estore-btn2c w3-padding">Add to cart</button>
+</div>
+</div>
+</div>
+    
+<div class="estore-dialog3">
+<div class="estore-dialog-content3">
+<img src="/images/estore-icon3c.png" style="width:20%;margin-top:3%" class="w3-circle estore-icon">
+<a href="#" class="estore-dialog-close w3-hover-opacity"><img src="/images/close.png" style="width:25px"></a>
+<div class="estore-dialog-text">
+<h4>Expression of Interest</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+</div>
+<div class="w3-center">
+<button class="estore-btn3c w3-padding">Add to cart</button>
+</div>
+</div>
+</div>
+
 <!-- Footer -->
 <?php returnRights(2,"",""); ?>
 </div>
