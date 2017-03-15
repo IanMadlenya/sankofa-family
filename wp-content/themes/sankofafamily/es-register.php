f5b5e36eb5d34cc56041e6da56c45ae72d199bfb
Jason Huang <jason.huang@contactspace.com>
2017-03-15 15:43:00 +1100

    Added validateForm()

--- wp-content/themes/sankofafamily/es-register.php
+++ wp-content/themes/sankofafamily/es-register.php
@@ -4,14 +4,37 @@
 <link rel="stylesheet" href="/css/w3.css">
 <link rel="stylesheet" href="/css/font-awesome.min.css">
 <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/jquery.min.js"></script>
+    <script>
+        function validateForm(){
+            var useremail = $('#username').val();
+            var userpwd = $('#espwd').val();
+            var cuserpwd = $('#cespwd').val();
+
+            if(!isValidEmailAddress(useremail)) {
+                return false;
+            } else if(!userpwd) {
+                return false;
+            } else if(userpwd != cuserpwd) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        function isValidEmailAddress(emailAddress) {
+            var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+            return pattern.test(emailAddress);
+        }
+    </script>
 </head>
 <body>
-<form id="regform">
+<form id="regform" action="/wp-content/themes/sankofafamily/es-register-insert.php" onsubmit="return validateForm()" method="post">
 <p>个人信息</p>
 <div class="w3-center" style="margin-bottom:30px">
-<p><input class="w3-input estore-input-login w3-opacity" type="text" name="username" placeholder="电子邮件（作为用户名）"></p>
-<p><input class="w3-input estore-input-login w3-opacity" type="password" name="espwd" placeholder="密码"></p>
-<p><input class="w3-input estore-input-login w3-opacity" type="password" name="cespwd" placeholder="确认密码"></p>
+<p><input class="w3-input estore-input-login w3-opacity" type="text" name="username" id="username" placeholder="电子邮件（作为用户名）"></p>
+<p><input class="w3-input estore-input-login w3-opacity" type="password" name="espwd" id="espwd" placeholder="密码"></p>
+<p><input class="w3-input estore-input-login w3-opacity" type="password" name="cespwd" id="cespwd" placeholder="确认密码"></p>
 </div>
 <p>信用卡资料 (可选填)</p>
 <div class="w3-center" style="margin-bottom:30px">
