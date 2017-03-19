7561dc406e5645b674dd9257369954f30d94b854
Jason Wong <jasonkwh@gmail.com>
2017-03-19 17:30:14 +1100

    Cart table design

--- wp-content/themes/sankofafamily/es-cart.php
+++ wp-content/themes/sankofafamily/es-cart.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/jquery.min.js"></script>
     <script src="/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="/css/carttable.css">
     <script>
         $(document).ready(function() {
             $('#carttable').dataTable({
@@ -57,7 +57,7 @@
         $CartPrice = "";
         
         if(($result) && ($result->num_rows !== 0)){ 
-            echo "<p><input class='w3-input estore-input-login w3-opacity' type='text' name='searchfilter' id='searchfilter' onkeyup='searchtable()' placeholder='search'></p><table id='carttable'><thead><tr><th>商品信息</th><th>单价</th><th>数量</th><th>金额</th><th>操作</th></tr></thead><tbody>";
+            echo "<p><input class='w3-input estore-input-login estore-input-cart w3-opacity' type='text' name='searchfilter' id='searchfilter' onkeyup='searchtable()' placeholder='search'></p><table id='carttable'><thead><tr><th>商品信息</th><th>单价</th><th>数量</th><th>金额</th><th>操作</th></tr></thead><tbody>";
             while($row = $result->fetch_assoc()) {
                 if($row['CartItemId'] == 1) {
                     $CartItemName = "Trust Setup Service";
