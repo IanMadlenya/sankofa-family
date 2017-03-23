5eedd60809249d433a9cd590f1d588851f930320
Jason Huang <jason.huang@contactspace.com>
2017-03-23 13:45:36 +1100

    Redirect to restore when trashed of a item

--- wp-content/themes/sankofafamily/es-trash.php
+++ wp-content/themes/sankofafamily/es-trash.php
@@ -6,8 +6,14 @@ if(isset($_SESSION['esuserid'])) {
     $itemid = $_GET['itemid'];
     $query = "UPDATE cs_cart SET Trash=1 WHERE Id=" . $itemid . " AND CustomerId=" . $_SESSION['esuserid'];
     $mysqli->query($query);
-
-    header( 'Location: /estore?cart&trashed' );
+    
+    $query = "select * from cs_cart where CustomerId=" . $_SESSION['esuserid'] . " and Sold=0 and Trash=0";
+    $result = $mysqli->query($query);
+    if(($result) && ($result->num_rows !== 0)){
+        header( 'Location: /estore?cart&trashed' );
+    } else {
+        header( 'Location: /estore?errorcart' );
+    }
 } else {
     header( 'Location: /estore' );
 }
