7cd0e64a7975ec957d35c1581113608c70de3b94
Jason Huang <jason.huang@contactspace.com>
2017-05-04 09:46:03 +1000

    use UTF-8 charset for email sending

--- wp-content/themes/sankofafamily/es-forgotemail.php
+++ wp-content/themes/sankofafamily/es-forgotemail.php
@@ -31,6 +31,7 @@ if(($result) && ($result->num_rows !== 0)){
     $mail = new PHPMailer();
     $body = "<img src='https://www.smfos.com.au/images/smfos_logo2.png' style='width:300px'><br><p>亲爱的 " . $username . " ，我们已为您随机生成了新的密码: </p><h3><strong>" . $randpwd . "</strong></h3><p>请使用以上密码再次登录 <a href='https://www.smfos.com.au/estore?login&loginname=" . $username . "&loginpwd=" . $randpwd . "'>eStore</a> ，并在设置页面重新设定您的个人密码。</p><p>如页面无法转接，请复制以下链接至浏览器地址栏，并点击回车键:</p><p><a href='https://www.smfos.com.au/estore?login&loginname=" . $username . "&loginpwd=" . $randpwd . "'>https://www.smfos.com.au/estore?login&loginname=" . $username . "&loginpwd=" . $randpwd . "</a></p><br><p>感谢您使用 SMFOS eStore!</p><p>SMFOS 团队敬上</p>";
     $mail->IsSMTP();
+    $mail->CharSet = 'UTF-8';
     $mail->Host = "smtp.exmail.qq.com";
     $mail->Port = "465";
     $mail->Username = "info@sankofafund.com.au";
@@ -38,8 +39,8 @@ if(($result) && ($result->num_rows !== 0)){
     $mail->SMTPSecure = "ssl";
     $mail->SMTPAuth = true;
     $mail->From = 'info@sankofafund.com.au';
-    $mail->FromName = 'Sankofa Multi-Family Offices';
-    $mail->Subject = "SMFOS eStore - Forgotten Password";
+    $mail->FromName = 'Sankofa 家族办公室';
+    $mail->Subject = "SMFOS eStore - 忘记密码";
     $mail->MsgHTML($body);
     $mail->addAddress($username);
     if(!$mail->Send()) {
