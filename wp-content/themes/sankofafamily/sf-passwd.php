ba8530aaa50a1051c30515ca37d6fc6af9902228
Jason Huang <jason.huang@contactspace.com>
2017-05-05 15:19:45 +1000

    keep credentials in the same place

--- wp-content/themes/sankofafamily/sf-passwd.php
+++ wp-content/themes/sankofafamily/sf-passwd.php
@@ -3,4 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "Sankofa809";
 $dbname = "sankofa-family";
+$phpmailhost = "smtp.exmail.qq.com";
+$phpmailport = "465";
+$phpmailaddress = "info@sankofafund.com.au";
+$phpmailpwd = "Sankofa1234";
+$phpmailsec = "ssl";
 ?>
